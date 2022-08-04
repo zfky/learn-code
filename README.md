826d4a4f8d25f06d76ed8d5347208451813850d1
zfky <wd928111@163.com>
2022-08-05 01:54:34 +0800

    修改jz链接位置

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # learn-code
 This repository is for learning how to code
+## [jz52](https://www.nowcoder.com/practice/6ab1d9a29e88450685099d45c9e31e46?tpId=13&tqId=23257&ru=/exam/oj/ta&qru=/ta/coding-interviews/question-ranking&sourceUrl=%2Fexam%2Foj%2Fta%3Fpage%3D1%26tpId%3D13%26type%3D13)
--- src/main/java/Jz52.java
+++ src/main/java/Jz52.java
@@ -1,5 +1,4 @@
 
-// https://www.nowcoder.com/practice/6ab1d9a29e88450685099d45c9e31e46?tpId=13&tqId=23257&ru=/exam/oj/ta&qru=/ta/coding-interviews/question-ranking&sourceUrl=%2Fexam%2Foj%2Fta%3Fpage%3D1%26tpId%3D13%26type%3D13
 public class Jz52 {
     public static void main(String[] args) {
 
