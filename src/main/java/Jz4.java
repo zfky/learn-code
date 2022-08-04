b2ad8926d84814bdfe0949a7e09cfb228fe527de
zfky <wd928111@163.com>
2022-08-05 01:47:36 +0800

    修改4问题中类表述，52问题初始导入

--- src/main/java/Jz4.java
+++ src/main/java/Jz4.java
@@ -3,12 +3,12 @@ public class Jz4 {
         int [][] array = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
         int target1 = 7;
         int target2 = 3;
-        Solution solution = new Solution();
+        Solution4 solution = new Solution4();
         System.out.println("7: " + solution.Find(target1,array) );
         System.out.println("3: " + solution.Find(target2,array) );
     }
 }
-class Solution {
+class Solution4 {
     public boolean Find(int target, int [][] array) {
         int i=0,j=array[0].length-1;
         while(i<array.length&&j>=0){
---
+++ src/main/java/Jz52.java
@@ -0,0 +1,21 @@
+
+// https://www.nowcoder.com/practice/6ab1d9a29e88450685099d45c9e31e46?tpId=13&tqId=23257&ru=/exam/oj/ta&qru=/ta/coding-interviews/question-ranking&sourceUrl=%2Fexam%2Foj%2Fta%3Fpage%3D1%26tpId%3D13%26type%3D13
+public class Jz52 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+class Solution52 {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        return null;
+    }
+}
