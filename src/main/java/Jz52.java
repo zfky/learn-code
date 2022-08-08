36d2f200b5b221b9181c17f81311590a61a31fb4
zfky <wd928111@163.com>
2022-08-09 00:23:41 +0800

    新增Jz52

--- src/main/java/Jz52.java
+++ src/main/java/Jz52.java
@@ -8,13 +8,42 @@ public class Jz52 {
 class ListNode {
     int val;
     ListNode next = null;
-
     ListNode(int val) {
         this.val = val;
     }
 }
 class Solution52 {
     public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        int i = 0;
+        int j = 0;
+        ListNode ph1 = pHead1;
+        ListNode ph2 = pHead2;
+        for(i=0;ph1!=null;i++){
+            ph1 = ph1.next;
+        }
+        for(j=0;ph2!=null;j++){
+            ph2 = ph2.next;
+        }
+        int k = i - j;
+        if(k>0){
+            for(int m=0;m<k;m++){
+                pHead1 = pHead1.next;
+            }
+        }
+        else{
+            for(int m=0;m<-k;m++){
+                pHead2 = pHead2.next;
+            }
+        }
+        while(pHead1!=null&&pHead2!=null){
+            if(pHead1.equals(pHead2)){
+                return pHead1;
+            }
+            else{
+                pHead1 = pHead1.next;
+                pHead2 = pHead2.next;
+            }
+        }
         return null;
     }
 }
