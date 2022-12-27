9162d20aab66471d320e13112d880a3ef05bdbe9
zfky <wd928111@163.com>
2022-12-27 22:39:50 +0800

    Jz23Solution

--- src/main/java/Jz23.java
+++ src/main/java/Jz23.java
@@ -1,8 +1,24 @@
 public class Jz23 {
 }
 class Solution23 {
-
     public ListNode EntryNodeOfLoop(ListNode pHead) {
-        return null;
+        ListNode fast = pHead;
+        ListNode low  = pHead;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            low = low.next;
+            if(fast == low){
+                break;
+            }
+        }
+        if(fast==null || fast.next==null){
+            return null;
+        }
+        low = pHead;
+        while(low!=fast){
+            low=low.next;
+            fast=fast.next;
+        }
+        return low;
     }
 }
