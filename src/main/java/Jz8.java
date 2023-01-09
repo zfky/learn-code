c284e8992e6869e314881ed377d718a71e063655
zfky <wd928111@163.com>
2023-01-09 23:56:29 +0800

    Jz8 init

---
+++ src/main/java/Jz8.java
@@ -0,0 +1,19 @@
+public class Jz8 {
+}
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
+class Solution8 {
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        return null;
+//        if(pNode != null && pNode.next)
+    }
+}
+
