75acf99813233a0a8154cd48ddc70e176fd6b468
zfky <wd928111@163.com>
2023-02-06 14:12:22 +0800

    Jz84 Solution

---
+++ src/main/java/Jz84.java
@@ -0,0 +1,25 @@
+public class Jz84 {
+}
+class Solution84 {
+    int res = 0;
+    public void dfs(TreeNode root, int sum){
+        if(root != null){
+            if(sum == root.val){
+                res++;
+            }
+            dfs(root.left,sum - root.val);
+            dfs(root.right,sum - root.val);
+        }
+    }
+    public int FindPath (TreeNode root, int sum) {
+        if(root == null){
+            return 0;
+        }
+        else{
+            dfs(root,sum);
+            FindPath(root.left,sum);
+            FindPath(root.right,sum);
+        }
+        return res;
+    }
+}
